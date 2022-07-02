edb23f97d61451e559800b8c910d316dbb895d0f
Vladyslav Ostapchuk <vladyslav.ostapchuk0@gmail.com>
2022-07-02 09:54:02 +0300

    test

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -22,7 +22,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
       {
         name: `${tour.name} Tour`,
         description: tour.summary,
-        images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+        images: [
+          `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`,
+        ],
         amount: tour.price * 100,
         currency: 'usd',
         quantity: 1,
@@ -37,20 +39,10 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   });
 });
 
-// exports.createBookingCheckout = catchAsync(async (req, res, next) => {
-//   // temporary
-//   const { tour, user, price } = req.query;
-
-//   if (!tour && !user && !price) return next();
-//   await Booking.create({ tour, user, price });
-
-//   res.redirect(req.originalUrl.split('?')[0]);
-// });
-
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].amount / 100;
+  const price = session.display_items[0].amount / 100;
   await Booking.create({ tour, user, price });
 };
 
@@ -71,7 +63,7 @@ exports.webhookCheckout = (req, res, next) => {
     createBookingCheckout(event.data.object);
 
   res.status(200).json({
-    recieved: true,
+    received: true,
   });
 };
 
